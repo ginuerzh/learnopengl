a327c3f22aa565cdf01b19d39aae314ba9ed935d
rui.zheng <rui.zheng@tcl.com>
2017-09-09 14:57:38 +0800

    texture exercises

---
+++ 1.getting_started/4.5.textures_exercise4/4.5.texture.fs
@@ -0,0 +1,18 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+uniform float mixRatio;
+
+void main()
+{
+	// linearly interpolate between both textures (80% container, 20% awesomeface)
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), vec4(texture(texture2, TexCoord)).a * mixRatio);
+}
